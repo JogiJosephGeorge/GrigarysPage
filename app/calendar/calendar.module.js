7f0af6b50a8dea767d085d0303a278d529deaee6
Jogi Joseph George <Jogi.George@kla-tencor.com>
2022-04-05 00:26:52 +0530

    Removed all

--- app/calendar/calendar.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Define the `calendar` module
-angular.module('calendar', ['core.calendar']);
--- app/core/calendar/calendar.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Define the `core.calendar` module
-angular.module('core.calendar', ['ngResource']);
